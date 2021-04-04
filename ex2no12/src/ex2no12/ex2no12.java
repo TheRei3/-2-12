bbe85da48f52cf75dda84a6c578085cd644657f7
admin <admin@DESKTOP-8L2IG92>
2021-04-04 09:36:01 +0900

    ex2no12

---
+++ ex2no12/src/ex2no12/ex2no12.java
@@ -0,0 +1,11 @@
+package ex2no12;
+
+public class ex2no12 {  //75p
+      public static void main(String[] args) {
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score = "+ score);
+		System.out.println("d = "+d);
+	}
+}
